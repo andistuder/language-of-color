605c8c3775c46898ededae271d06b3c20e9f8543
Andi Studer <andi.studer@fundingcircle.com>
2015-02-09 17:07:37 +0000

    refactor devise views, hook into members page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    additional_fields = [
+      :first_name,
+      :last_name,
+      :title,
+      :country_of_residence,
+      :job_title,
+      :organisation,
+      :link
+    ]
+    devise_parameter_sanitizer.for(:sign_up).push(additional_fields)
+    devise_parameter_sanitizer.for(:account_update).push(additional_fields)
+  end
 end
