91866cdc623f8a57b6ab2eb57699d099e6208603
andistuder <andi@cenatus.org>
2015-02-09 23:08:24 +0000

    add static bibliography

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,6 @@ class PagesController < ApplicationController
   end
   def about
   end
+  def bibliography
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about' => 'pages#about'
+  get 'bibliography' => 'pages#bibliography'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
