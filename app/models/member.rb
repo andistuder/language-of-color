f553df77ed323c1ca21273b65de25c6978a515c4
andistuder <andi@cenatus.org>
2015-02-12 16:47:32 +0000

    default order by first name

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,6 @@ class Member < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  default_scope { order('first_name') }
 end
