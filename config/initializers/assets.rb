b57eff529e4938efa0fadd1d4c9a668dd902cc44
Andi Studer <andi.studer@fundingcircle.com>
2015-03-10 13:53:17 +0000

    Revert "update asset config"
    This reverts commit 70d362621c83ef97687903ca3285f544340985b9.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '2.1'
+Rails.application.config.assets.version = '2.2'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( font-awesome.css )
+Rails.application.config.assets.precompile += %w( font-awesome.css )
