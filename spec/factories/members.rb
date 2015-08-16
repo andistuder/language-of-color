8b9a0554530025322f4816ffe387579ab10bc2e1
andistuder <andi@cenatus.org>
2015-08-16 20:46:27 +0100

    add factory for admin

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -7,11 +7,10 @@ FactoryGirl.define do
     link { "http://www.#{last_name}-industries.com".downcase }
     sequence(:email) { |n| "#{first_name}.#{last_name}-#{n}@example.com".downcase }
     is_admin false
-  end
 
-  factory :admin, class: Member do
-    first_name 'Admin'
-    last_name 'User'
-    is_admin true
+    trait :admin do
+       is_admin true
+     end
+     factory :admin, traits: [:admin]
   end
 end
