e10e8bc384adb2671e76d0dc74c507b44c738c4a
andistuder <andi@cenatus.org>
2015-12-01 07:21:42 +0000

    disable automatic member registration
    whilst we investigate spam issues

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,8 +2,8 @@ require 'csv'
 
 class Member < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, :timeoutable, :registerable and :omniauthable
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   default_scope { order('first_name') }
