369e106eb1bed963e34bf1ae691fd634c3800e31
Andi Studer <andi.studer@fundingcircle.com>
2016-01-09 14:43:26 +0000

    refactor tests to cover nav items

--- features/step_definitions/pages_steps.rb
+++ features/step_definitions/pages_steps.rb
@@ -7,7 +7,14 @@ When 'I visit "$path"' do |path|
 end
 
 Then 'I can see the navigation' do
-  expect(page).to have_css('ul.nav')
+  navigation_items = page.find_all('ul.nav li')
+  expect(navigation_items.length).to eq(6)
+  expect(navigation_items[0]).to have_xpath('.//a[@href="/about"]', text: 'About')
+  expect(navigation_items[1]).to have_xpath('.//a[@href="/members"]', text: 'Members')
+  expect(navigation_items[2]).to have_xpath('.//a[@href="/bibliography"]', text: 'Bibliography')
+  expect(navigation_items[3]).to have_xpath('.//a[@href="/resources"]', text: 'Resources')
+  expect(navigation_items[4]).to have_xpath('.//a[@href="/contact"]', text: 'Contact')
+  expect(navigation_items[5]).to have_xpath('.//a[@href="/members/sign_in"]', text: 'Log in')
 end
 
 Then 'I can see the page heading "$heading"' do |heading|
